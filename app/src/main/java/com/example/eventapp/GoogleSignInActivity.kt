f6cc7cc14bbf99ccbec19e45f67d6c3802516bcb
zehrash <zehrash11@gmail.com>
2020-09-15 17:26:32 +0300

    Fix Firebase

--- app/src/main/java/com/example/eventapp/GoogleSignInActivity.kt
+++ app/src/main/java/com/example/eventapp/GoogleSignInActivity.kt
@@ -1,26 +1,53 @@
 package com.example.eventapp
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.SignInButton
+import com.google.android.gms.tasks.OnCompleteListener
 
 
 class GoogleSignInActivity : AppCompatActivity() {
     //val signIn: Button
+    // private lateinit var mGoogleSignInClient: GoogleSignInClient
+/*
+   private lateinit var mGoogleSignInClient:GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_google_sign_in)
+
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
             .build()
         val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-      //  val listener = findViewById<View>(R.id.sign_in_button).setOnClickListener(this)
 
+        val name: TextView = findViewById(R.id.name)
+        val email: TextView = findViewById(R.id.email)
+        val id: TextView = findViewById(R.id.id)
+        val acct: GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(this)
+
+        val personName: String = acct?.displayName.toString()
+        val personEmail: String = acct?.email.toString();
+        val personId: String = acct?.id.toString()
+        // Uri personPhoto = acct . getPhotoUrl ();
+
+
+        name.text = personName
+        email.text = personEmail
+        id.text = personId
     }
 
+    private fun signOut() {
+        mGoogleSignInClient.signOut()
+            .addOnCompleteListener(this, OnCompleteListener<Void?> {
+                // ...
+            })
+    }
+
+*/
     //check for an existing signed-n user
 }
--- app/src/main/java/com/example/eventapp/MainActivity.kt
+++ app/src/main/java/com/example/eventapp/MainActivity.kt
@@ -1,12 +1,93 @@
 package com.example.eventapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
+
+    //private val RC_SIGN_IN = 1
+    //private lateinit var mGoogleSignInClient: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+      //  mAuth = FirebaseAuth.getInstance()
+        /*
+        val button: SignInButton = findViewById(R.id.sign_in_button)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        button.setOnClickListener() {
+            signIn();
+        }
+
     }
-}
+
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(
+            signInIntent, RC_SIGN_IN
+        )
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task =
+                GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(
+                ApiException::class.java
+            )
+            val googleId = account?.id ?: ""
+            Log.i("Google ID", googleId)
+
+            val googleFirstName = account?.givenName ?: ""
+
+            Log.i("Google First Name", googleFirstName)
+
+            val googleLastName = account?.familyName ?: ""
+            Log.i("Google Last Name", googleLastName)
+
+            val googleEmail = account?.email ?: ""
+            Log.i("Google Email", googleEmail)
+
+        } catch (e: ApiException) {
+            Log.e(
+                "failed code=", e.statusCode.toString()
+            )
+        }
+    }
+
+    private fun signOut() {
+        mGoogleSignInClient.signOut()
+            .addOnCompleteListener(this) {
+                // Update your UI here
+            }
+    }
+
+         */
+
+
