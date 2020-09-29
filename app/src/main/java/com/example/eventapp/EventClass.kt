ffcc862675a163fdc3443e70cc036644dda2cfc8
zehrash <zehrash11@gmail.com>
2020-09-29 17:52:11 +0300

    Add requests from api Fix dependencies

--- app/src/main/java/com/example/eventapp/EventClass.kt
+++
@@ -1,7 +0,0 @@
-package com.example.eventapp
-
-data class EventClass(
-    val name:String,
-    val location:String,
-    val performance:String
-)
--- app/src/main/java/com/example/eventapp/EventRequest.kt
+++ app/src/main/java/com/example/eventapp/EventRequest.kt
@@ -1,5 +1,6 @@
 package com.example.eventapp
 
+import android.provider.ContactsContract
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
@@ -16,7 +17,7 @@ class EventRequest(val apiKey: String, val client: OkHttpClient) {
     fun getEvent()/*: MutableList<EventClass>*/ {
         val url = apiUrl + "/events?client_id=" + apiKey
         val request: Request = Request.Builder().url(url).build()
-        var eventList: List<EventClass>
+        var event: DataClass
         val call = client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
                 println("Failed to execute task")
@@ -25,13 +26,27 @@ class EventRequest(val apiKey: String, val client: OkHttpClient) {
             override fun onResponse(call: Call, response: Response) {
                 val body = response.body()?.string()
                 val gson = GsonBuilder().create()
-                val mutableListTutorialType = object : TypeToken<MutableList<EventClass>>() {}.type
-                eventList = gson.fromJson(body, mutableListTutorialType)
-                //  eventList = listOf(gson.fromJson(body, EventClass::class.java))
+                //val mutableListTutorialType = object : TypeToken<MutableList<DataClass>>() {}.type
+
+                 event= gson.fromJson(body, DataClass::class.java)
             }
         })
         //return eventList
     }
+/*
+    fun getEventByVenue(type: String, keyword:String): MutableList<DataClass>{
+        val url: String = apiUrl+ "/events?venue." + "${type}=${keyword}&client_id=" + apiKey
 
+        val request: Request= Request.Builder().url(url).build()
+        val call = client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to exec task")
+            }
 
+            override fun onResponse(call: Call, response: Response) {
+               val body = response.body()?.string()
+            }
+        }
+
+*/
 }
